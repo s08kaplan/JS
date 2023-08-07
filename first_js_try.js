a31c667a316ac0118e5c394adb568881c8b93796
Serkan <s8ersis@gmail.com>
2023-08-07 06:22:32 +0300

    second day

--- first_js_try.js
+++ first_js_try.js
@@ -66,3 +66,25 @@ function checkLetters(val) {
 console.log(checkLetters("a"));
 console.log(checkLetters("c"));
 console.log(checkLetters("3"));
+
+//---------------------------------------
+
+function isLess(a,b) {
+    return a < b;
+}
+
+console.log(isLess(25,33));
+console.log(isLess(43,33));
+//-----------------------------
+
+function abTest(a,b) {
+    if (a < 0 || b < 0) {
+        return undefined;
+    }
+    return Math.round(Math.pow(Math.sqrt(a) + Math.sqrt(b), 2));
+}
+console.log(abTest(2,2));
+console.log(abTest(-2,2));
+//-------------------------------
+
+
